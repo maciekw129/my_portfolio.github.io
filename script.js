173f5c9fad2c611324bced5e75b0ad3bbfebd819
Maciej Walecki <maciejwalecki99@gmail.com>
2021-03-02 08:58:19 +0100

    Add complete header

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const menuButton = document.getElementById('menu_button');
-const nav = document.querySelector('nav');
 
-const viewMenu = () => {
-    nav.style.display = 'block';
-    menuButton.addEventListener('mouseout', hideMenu);
-    nav.onmouseout = hideMenu;
-}
-
-const hideMenu = () => {
-    nav.style.display = 'none';
-}
-
-menuButton.addEventListener('mouseover', viewMenu);
-
-nav.onmouseover = viewMenu;
