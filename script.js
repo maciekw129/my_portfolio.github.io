51898261fdb7196d822a50e72634af612b48440e
Maciej Walecki <maciejwalecki99@gmail.com>
2021-03-07 17:41:09 +0100

    Complete site without mobile version

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const floraTitle = document.getElementById('floraTitle');
+const kickboxingTitle = document.getElementById('kickboxingTitle');
+const floraJpg = document.getElementById('flora');
+const kickboxingJpg = document.getElementById('kickboxing');
+const kickboxingArrow = document.getElementById('kickboxingArrow');
+const floraArrow = document.getElementById('floraArrow');
+
+
+
+const show = (picture, title, arrow) => {
+    arrow.style.transform = 'rotate(180deg)';
+    picture.style.maxHeight = '400px';
+        title.removeEventListener('click', function(){
+        show(picture);
+    })
+    title.addEventListener('click', function(){
+        hide(picture, title, arrow);
+    })    
+}
+
+const hide = (picture, title, arrow) => {
+    arrow.style.transform = 'rotate(0)';
+    picture.style.maxHeight = 0;
+    title.removeEventListener('click', function(){
+        hide(picture);
+    })
+    title.addEventListener('click', function(){
+        show(picture, title, arrow);
+    })
+}
+
+floraTitle.addEventListener('click', function(){
+    show(floraJpg, floraTitle, floraArrow);
+})
+
+kickboxingTitle.addEventListener('click', function(){
+    show(kickboxingJpg, kickboxingTitle, kickboxingArrow);
+})
 
